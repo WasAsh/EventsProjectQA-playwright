d0dc75d8f0bbcce70937eecda81ff822b99b94ee
WasAsh <wasee.2132254@gmail.com>
2026-07-01 17:14:28 +0300

    Navigate to base URL after user registration

--- tests/regesteration.spec.ts
+++ tests/regesteration.spec.ts
@@ -13,6 +13,7 @@ test.describe('User Regesteration Tests', () => {
         await page.goto(`${ENV.baseURL}/register`);
         await expect(registerUserPage.textLabel).toBeVisible();
         await registerUserPage.addNewUser(USERS.user.email , USERS.user.password);
+        await page.goto(ENV.baseURL);
         await expect(page).toHaveURL(ENV.baseURL);
 
       });
